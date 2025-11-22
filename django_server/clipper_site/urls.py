8b093fff3828cec26e036295b1b38fb27c7d4c32
Shaik Ikbhal Basha <theikbhal@gmail.com>
2025-11-22 11:06:32 +0530

    update clips

--- django_server/clipper_site/urls.py
+++ django_server/clipper_site/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clips.urls')),
+    path('clips/', include('clips.urls')),
     path("go/", include("golinks.urls")),
 ]
